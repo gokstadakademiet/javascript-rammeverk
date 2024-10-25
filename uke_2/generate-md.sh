555cace1893d6baf3e14ac6b223a65121efa254a
Andreas Wilhelm Flått <andreasflaatt@gmail.com>
2024-10-25 14:14:38 +0200

    Add utfordringsoppgaver

--- uke_2/generate-md.sh
+++ uke_2/generate-md.sh
@@ -21,12 +21,6 @@ echo "# Innholdsfortegnelse
         7. [Oppgave 7: Avhengigheter i \`useEffect\`](#oppgave-7-avhengigheter-i-useeffect)
         8. [Oppgave 8: Avhengigheter med komplekse objekter](#oppgave-8-avhengigheter-med-komplekse-objekter)
         9. [Oppgave 9: Avhengigheter med flere effekter](#oppgave-9-avhengigheter-med-flere-effekter)
-        10. [Oppgave 10: Tidsbasert oppdatering](#oppgave-10-tidsbasert-oppdatering)
-        11. [Oppgave 11: Formhåndtering](#oppgave-11-formhåndtering)
-        12. [Oppgave 12: Avbryte API-kall](#oppgave-12-avbryte-api-kall)
-        13. [Oppgave 13: Debouncing med \`useEffect\`](#oppgave-13-debouncing-med-useeffect)
-        14. [Oppgave 14: Dynamisk bakgrunnsfarge](#oppgave-14-dynamisk-bakgrunnsfarge)
-        15. [Oppgave 15: Synkronisere tilstand med URL-parametere](#oppgave-15-synkronisere-tilstand-med-url-parametere)
 2. [Feilsøkingsoppgaver](#feilsøkingsoppgaver)
     1. [Feilsøkingsoppgave 1](#feilsøkingsoppgave-1)
     2. [Feilsøkingsoppgave 2](#feilsøkingsoppgave-2)
@@ -34,6 +28,13 @@ echo "# Innholdsfortegnelse
     4. [Feilsøkingsoppgave 4](#feilsøkingsoppgave-4)
     5. [Feilsøkingsoppgave 5](#feilsøkingsoppgave-5)
     6. [Feilsøkingsoppgave 6](#feilsøkingsoppgave-6)
+3. [Utfordringsoppgaver](#utfordringsoppgaver)
+    1. [Oppgave 1: Tidsbasert oppdatering](#oppgave-1-tidsbasert-oppdatering)
+    2. [Oppgave 2: Formhåndtering](#oppgave-2-formhåndtering)
+    3. [Oppgave 3: Avbryte API-kall](#oppgave-3-avbryte-api-kall)
+    4. [Oppgave 4: Debouncing med \`useEffect\`](#oppgave-4-debouncing-med-useeffect)
+    5. [Oppgave 5: Dynamisk bakgrunnsfarge](#oppgave-5-dynamisk-bakgrunnsfarge)
+    6. [Oppgave 6: Synkronisere tilstand med URL-parametere](#oppgave-6-synkronisere-tilstand-med-url-parametere)
 " >> "$TARGET_FILE"
 
 for md_file in $(ls "$SOURCE_DIR"/*.md | sort); do
